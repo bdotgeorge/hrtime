a73f85af4c26ed19d9f6949deac6f878fd417087
George B <bzzz2009@yandex.ru>
2019-10-03 11:05:24 +0400

    second day break brain

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ vector<string> split(string s, char c) {
     vector<string> splittedstring;
     string tempString;
     for (int i = 0; i <= s.size(); ++i) {
-        if (s[i] != c && i != s.size() && s[i]!='\n') {
+        if (s[i] != c && i != s.size() && s[i] != '\n') {
             tempString += s[i];
         } else {
             splittedstring.push_back(tempString);
@@ -22,52 +22,67 @@ vector<string> split(string s, char c) {
 }
 
 vector<string> splitForWord(string s, string w) {
-    vector<string> splittedstring;
-    vector<string> tempvector = split(s, '\n');
+    setlocale(LC_ALL, "RUS");
+    //s.replace(s.begin(), s.end(), "\r", "\t");
+    //s.replace(s.begin(), s.end(), "\n", "\t");
+    vector<string> splittedString;
+    vector<string> tempVector = split(s, '\t');
     string tempString;
-    for (string st: tempvector){
-        // to split everything by vords -
+    for (int i = 0; i < tempVector.size(); ++i) {
+        if (tempVector[i] != w && i != tempVector.size() - 1) {
+            tempString += tempVector[i];
+
+        } else {
+            splittedString.push_back(tempString);
+            splittedString.push_back("\n");
+            tempString = "";
+            continue;
+        }
     }
 
-    return splittedstring;
+    return splittedString;
 }
 
 
-int toMinutten(string &s){
-    int h = stoi(split(s,':')[0]);
-    int  min = stoi(split(s,':')[1]);
-    return  h*60 + min;
+int toMinutten(string &s) {
+    int h = stoi(split(s, ':')[0]);
+    int min = stoi(split(s, ':')[1]);
+    return h * 60 + min;
 }
-string toNormalHoursAndMinutes(int fullMinutes){
+
+string toNormalHoursAndMinutes(int fullMinutes) {
     string s;
-    s+= to_string(fullMinutes / 60);
-    s+=":";
-    s+= to_string(fullMinutes % 60);
+    s += to_string(fullMinutes / 60);
+    s += ":";
+    s += to_string(fullMinutes % 60);
     return s;
 }
 
-pair<string, int> basicStringTokenaser(const string& stt) {
+pair<string, int> basicStringTokenaser(const string &stt) {
     pair<string, int> today;
     today.first = split(stt, '\t')[2];
-    today.second =  toMinutten(split(stt, '\t')[split(stt, '\t').size()-1]);
+    today.second = toMinutten(split(stt, '\t')[split(stt, '\t').size() - 1]);
     return today;
 }
 
-void pairPrint(const pair<string, int> &para){
+void pairPrint(const pair<string, int> &para) {
     cout << para.first << "  " << toNormalHoursAndMinutes(para.second);
 }
 
 
-
-vector<string> everyPerson(string s){
+vector<string> everyPerson(string s) {
 
 
 }
 
 int main() {
-string s;
+    setlocale(LC_ALL, "RUS");
+    string s;
 getline(cin, s);
-pairPrint(basicStringTokenaser(s));
+//    cin >> s;
+//pairPrint(basicStringTokenaser(s));
+    !splitForWord(s, "времени:").empty() ? cout << splitForWord(s, "времени:")[0] << splitForWord(s, "времени:").size()
+                                         : cout << "NO";
 
     return 0;
 }
